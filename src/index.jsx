a5730593d24f7d82a887a2d39f874f10812d5ac1
andreachiodini <a.chiodini01@gmail.com>
2025-04-27 17:45:22 +0200

    Create index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CollaboratoriLogin from './CollaboratoriLogin';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <CollaboratoriLogin />
+  </React.StrictMode>
+);
